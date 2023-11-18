cdcac65a4e36492cbd4b6138a22af6c370107266
Fabiano Oliveira <fabiano.one@gmail.com>
2023-11-18 16:58:59 -0300

    activate, deactivate and uninstall methods

--- mv-slider.php
+++ mv-slider.php
@@ -44,9 +44,24 @@ if( ! class_exists( 'MV_Slider' ) ){
             define( 'MV_SLIDER_URL', plugin_dir_url( __FILE__ ) );
             define( 'MV_SLIDER_VERSION', '1.0.0' );
         }
+
+        public static function activate(){
+            update_option( 'rewrite_rules', '' );
+        }
+
+        public static function deactivate(){
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall(){
+
+        }
     }
 }
 
 if( class_exists( 'MV_Slider') ){
+    register_activation_hook( __FILE__, array( 'MV_Slider', 'activate' ) );
+    register_deactivation_hook( __FILE__, array( 'MV_Slider', 'deactivate' ) );
+    register_uninstall_hook( __FILE__, array( 'MV_Slider', 'uninstall' ) );
     $mv_slider = new MV_Slider();
 }
