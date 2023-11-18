7c2c513817ad403b15e0aaee9c4fe1ebc3bbf8b4
Fabiano Oliveira <fabiano.one@gmail.com>
2023-11-18 15:22:27 -0300

    Plugin Structure

---
+++ mv-slider.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name:       MV Slider
+ * Plugin URI:        https://github.com/fabianoone/mv-slider
+ * Description:       A WordPress slider plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Fabiano Oliveira
+ * Author URI:        https://github.com/fabianoone
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       mv-slider
+ * Domain Path:       /languages
+ */
+/*
+MV Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+MV Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MV Slider. If not, see {URI to Plugin License}.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
