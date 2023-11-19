415cfb34e3280f5dc8d1349048ce606c647adbad
Fabiano Oliveira <fabiano.one@gmail.com>
2023-11-19 18:22:24 -0300

    Validating and sanitizing data

--- post-types/class.mv-slider-cpt.php
+++ post-types/class.mv-slider-cpt.php
@@ -59,12 +59,21 @@ if( ! class_exists( 'MV_Slider_Post_Type') ){
         {
             if( isset( $_POST['action'] ) && $_POST['action']  == 'editpost' ){
                 $old_link_text = get_post_meta( $post_id, 'mv_slider_link_text', true);
-                $new_link_text = $_POST['mv_slider_link_text'];
+                $new_link_text = sanitize_text_field( $_POST['mv_slider_link_text'] );
                 $old_link_url = get_post_meta( $post_id, 'mv_slider_link_url', true);
-                $new_link_url = $_POST['mv_slider_link_url'];
+                $new_link_url = esc_url_raw( $_POST['mv_slider_link_url'] );
 
-                update_post_meta( $post_id, 'mv_slider_link_text', $new_link_text, $old_link_text);
-                update_post_meta( $post_id, 'mv_slider_link_url', $new_link_url, $old_link_url);
+                if( empty( $new_link_text) ){
+                    update_post_meta( $post_id, 'mv_slider_link_text', 'Add some text');
+                } else {
+                    update_post_meta( $post_id, 'mv_slider_link_text', $new_link_text, $old_link_text);
+                }
+
+                if( empty( $new_link_url) ){
+                    update_post_meta( $post_id, 'mv_slider_link_url', '#');
+                } else {
+                    update_post_meta( $post_id, 'mv_slider_link_url', $new_link_url, $old_link_url);
+                }
             }
         }
     }
